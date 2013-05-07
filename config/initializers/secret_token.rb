8cac739e4b5852785ef237fbc482004baba3dfdd
John Maddux <jemaddux@gmail.com>
2013-05-07 15:21:22 -0600

    FIRST

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EspnApiExplorer::Application.config.secret_token = 'f593c2d581e785f270b676ecabd95cd490bc7a2741fe238b0e4328e1757e17a1a07a1ecba5ac680727b78366a5fcc77a8b278144594b3a49cb3ce2cfc5b92565'
